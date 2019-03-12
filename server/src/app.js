b67653d03caf6f593ebca06908845a35c08ae95f
BennoKrojer <benno.krojer@gmail.com>
2019-03-12 16:05:37 +0000

    using Mongo Atlas cluster uri instead of local db in server/app.js

--- server/src/app.js
+++ server/src/app.js
@@ -4,9 +4,11 @@ const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const config = require('./config/config')
+const MongoClient = require('mongodb').MongoClient;
 
-mongoose.connect('mongodb://localhost:27017/medcal', {useNewUrlParser: true});
 
+//mongoose.connect('mongodb://localhost:27017/medcal', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://Anton:medcal@medcal-pwjx5.mongodb.net/medcal', {useNewUrlParser: true});
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
