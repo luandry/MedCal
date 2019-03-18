f7eada9ebcd47916947cee0b99a8d33d36357c4d
BennoKrojer <benno.krojer@gmail.com>
2019-03-18 22:20:21 +0000

    search query can now handle address searches as well

--- server/src/models/Doctor.js
+++ server/src/models/Doctor.js
@@ -10,7 +10,53 @@ const Doctor = new mongoose.Schema({
     type: String,
     trim: true,
     default: ''
-  }
+  },
+  doctortype: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  address: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  cord: {
+    lang: {
+      type: String,
+      trim: true,
+      default: 0
+    },
+    lat: {
+      type: String,
+      trim: true,
+      default: 0
+    }
+  },
+  phone: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  mail: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  calendar:
+  [
+    { start: {
+      type: Date,
+      trim: true,
+      default: Date.now
+      },
+      end: {
+        type: Date,
+        trim: true,
+        default: Date.now
+      }
+    }
+  ]
 })
 
 module.exports = mongoose.model('Doctor', Doctor)
