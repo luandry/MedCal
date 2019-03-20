6fb3353dea2a7ffa3cc5162f9ca0f4fd4339730c
BennoKrojer <benno.krojer@gmail.com>
2019-03-20 17:46:53 +0000

    see bio/description of doctor in search results

--- client/src/components/Search/SearchResultsPanel.vue
+++ client/src/components/Search/SearchResultsPanel.vue
@@ -10,7 +10,7 @@
         </template>
 
         <v-card>
-          <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+          <v-card-text>{{doctor.bio}}</v-card-text>
           <v-layout justify-center>
             <v-btn
               class="blue"
