5652eef17ff9fea139e719095142ef16e852b07e
zuo132 <47151679+zuo132@users.noreply.github.com>
2019-03-27 10:00:58 +0000

    heroku

--- server/src/app.js
+++ server/src/app.js
@@ -4,11 +4,12 @@ const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const config = require('./config/config')
+const path = require('path')
 const MongoClient = require('mongodb').MongoClient;
 
 mongoose.connect('mongodb+srv://Anton:medcal@medcal-pwjx5.mongodb.net/medcal', {useNewUrlParser: true});
 const app = express()
-app.use(express.static(__dirname + '/dist/'))
+app.use(express.static(path.resolve(__dirname + '/../../dist/')))
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
